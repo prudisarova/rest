b995ce33f79a1a87769b0861944b9877ce3d003a
prudisarova <75304343+prudisarova@users.noreply.github.com>
2025-09-15 00:12:04 +0200

    Added Betterstack logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,3 +1,5 @@
 import { Logtail } from "@logtail/node";
 
-export const logger = new Logtail(process.env.LOGTAIL_TOKEN);
+export const logger = new Logtail(process.env.LOGTAIL_TOKEN, {
+  endpoint: "https://s1518531.us-east-9.betterstackdata.com",
+});
